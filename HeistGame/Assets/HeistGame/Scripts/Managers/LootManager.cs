9b0e0f8e1d33a3c4bec159d0af8c3524ac6f7c10
flickenmaste <will.gilstrap@gmail.com>
2015-02-06 17:07:02 -0800

    Added loot man, and picking up gold with player

---
+++ HeistGame/Assets/HeistGame/Scripts/Managers/LootManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LootManager : MonoBehaviour {
+
+    // Nodes for loot manager
+    public GameObject LootSpawn;
+    public GameObject DropOffPoint;
+
+    // bool to check if loot picked up or not
+    public bool LootTaken;
+
+    // Loot to spawn
+    public GameObject Loot;
+
+	// Use this for initialization
+	void Start () 
+    {
+        LootTaken = false;
+        LootSpawn = GameObject.FindGameObjectWithTag("LootManSpawn");
+        DropOffPoint = GameObject.FindGameObjectWithTag("LootManDropOff");
+        SpawnLoot();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+    void SpawnLoot()
+    {
+        GameObject clone = Instantiate(Loot, LootSpawn.transform.position, Quaternion.identity) as GameObject;
+    }
+
+    void LootIsTaken()
+    {
+
+    }
+}
---
+++ HeistGame/Assets/HeistGame/Scripts/Player/Player.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    // Bool to show player is carrying gold
+    public bool CarryingGold;
+
+    public GameObject LootMan;
+
+    public GameObject Loot;
+    
+    // Use this for initialization
+	void Start () 
+    {
+        CarryingGold = false;
+        LootMan = GameObject.FindGameObjectWithTag("LootManager");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        CheckInput();
+	}
+
+    void CheckInput()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+            PickUpGold();
+
+        if (Input.GetKeyUp(KeyCode.G))
+            DropGold();
+    }
+
+    void PickUpGold()
+    {
+        RaycastHit Hit;	// Store raycast info
+
+        Vector3 dir = new Vector3(transform.forward.x, transform.forward.y - 0.5f, transform.forward.z);
+
+        Debug.DrawRay(this.transform.position, dir * 2, Color.red);	// Ray so we can see
+
+        if (Physics.Raycast(transform.position, dir, out Hit, 2))
+        {
+            if (Hit.transform.tag == "Loot")
+            {
+                LootMan.gameObject.GetComponent<LootManager>().LootTaken = true;
+                Loot = Hit.transform.gameObject;
+                Loot.GetComponent<GoldLoot>().Taken = true;
+                Loot.GetComponent<GoldLoot>().HideLoot();
+                CarryingGold = true;
+            }
+        }
+    }
+
+    void DropGold()
+    {
+        if (CarryingGold)
+        {
+            LootMan.gameObject.GetComponent<LootManager>().LootTaken = false;
+            Loot.GetComponent<GoldLoot>().Taken = false;
+            Loot.GetComponent<GoldLoot>().HideLoot();
+            CarryingGold = false;
+            Loot.transform.position = transform.position;
+        }
+    }
+}
