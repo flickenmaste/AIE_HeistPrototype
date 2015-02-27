9ededc424d65fa2eb13f6a1a30c039135635a562
ForceOfEvil <derekpotter@msn.com>
2015-02-27 14:09:23 -0800

    The Death Wall

---
+++ HeistGame/Assets/WallKill.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallKill : MonoBehaviour {
+
+	public Transform Player;
+
+
+	void OnTriggerEnter(Collider coll)
+	{
+		if (coll.attachedRigidbody == Player.rigidbody)
+						Application.LoadLevel (Application.loadedLevel);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
