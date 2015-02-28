44df1b126324a295e52c9657596d61132adda27e
Brandon Foote <brandon.foote95@gmail.com>
2015-02-27 16:30:21 -0800

    Polishing demo
    step 1

---
+++ HeistGame/Assets/Endpoint/Scripts/WinLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLevel : MonoBehaviour {
+
+	[SerializeField]
+	public string LevelToLoad;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.gameObject.tag == "Player")
+						Application.LoadLevel (LevelToLoad);
+	}
+}
