beaffcf23a6974bf297bb77e26890fb3321913dc
flickenmaste <will.gilstrap@gmail.com>
2015-03-13 11:56:58 -0700

    working on server authoriative

--- HeistGame/Assets/HeistGame/Networking/Scripts/NetworkCallbacks.cs
+++ HeistGame/Assets/HeistGame/Networking/Scripts/NetworkCallbacks.cs
@@ -11,29 +11,6 @@ public class NetworkCallbacks : Bolt.GlobalEventListener
 
     public override void SceneLoadLocalDone(string map) // When player loads a level
     {
-        // randomize a position
-        //var pos = new Vector3(0, 2, -191);
-        //var pos = new Vector3(5, 2, -19);
 
-        // instantiate cube
-        //BoltNetwork.Instantiate(BoltPrefabs.PlayerEndpoint, pos, Quaternion.identity);
-
-        if (map == "HostLobby") // Check map
-            Debug.Log("Lobby Started");
-
-        if (map == "TestNetworkedLevel")
-        {
-            var pos = new Vector3(0, 2, 0);
-            BoltNetwork.Instantiate(BoltPrefabs.PlayerNetworked, pos, Quaternion.identity);
-            
-            if (BoltNetwork.isServer)
-            {
-                var civ1 = new Vector3(32, 1.3f, -21);
-                BoltNetwork.Instantiate(BoltPrefabs.SimpleCivNetworked, civ1, Quaternion.identity);
-
-                var civ2 = new Vector3(32, 1.3f, -25);
-                BoltNetwork.Instantiate(BoltPrefabs.SimpleCivNetworked, civ2, Quaternion.identity);
-            }
-        }
     }
 }
--- HeistGame/Assets/HeistGame/Networking/Scripts/ServerCallback.cs
+++ HeistGame/Assets/HeistGame/Networking/Scripts/ServerCallback.cs
@@ -1,8 +1,83 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerObject
+{
+    public BoltEntity character;
+    public BoltConnection connection;
+
+    public bool isServer
+    {
+        get { return connection == null; }
+    }
+
+    public bool isClient
+    {
+        get { return connection != null; }
+    }
+
+    public void Spawn()
+    {
+        var pos = new Vector3(0, 2, 0);
+
+        if (!character)
+        {
+            character = BoltNetwork.Instantiate(BoltPrefabs.PlayerNetworked, pos, Quaternion.identity);
+
+            if (isServer)
+            {
+                character.TakeControl();
+            }
+            else
+            {
+                character.AssignControl(connection);
+            }
+        }
+    }
+}
 
 [BoltGlobalBehaviour(BoltNetworkModes.Server)]
 public class ServerCallback : Bolt.GlobalEventListener
 {
-    // uhgughughughughu
+    static List<PlayerObject> players = new List<PlayerObject>();
+
+    void Awake()
+    {
+        PlayerObject p;
+        p = new PlayerObject();
+        p.connection = null;
+
+        players.Add(p);
+    }
+
+    public override void Connected(BoltConnection arg)
+    {
+        PlayerObject p;
+        p = new PlayerObject();
+        p.connection = arg;
+        p.connection.UserData = p;
+
+        players.Add(p);
+    }
+
+    public override void SceneLoadLocalDone(string map)
+    {
+        players[0].Spawn();
+    }
+
+    public override void SceneLoadRemoteDone(BoltConnection connection)
+    {
+        GetPlayer(connection).Spawn();
+    }
+
+    public static PlayerObject GetPlayer(BoltConnection connection)
+    {
+        if (connection == null)
+        {
+            // blah
+        }
+
+        return (PlayerObject)connection.UserData;
+    }
 }
