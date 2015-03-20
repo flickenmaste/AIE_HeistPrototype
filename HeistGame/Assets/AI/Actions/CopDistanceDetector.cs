e6a56bb7cf497634ade3b5b0ad88c08af9f7b53f
ForceOfEvil <derekpotter@msn.com>
2015-03-20 15:36:05 -0700

    Just a commit before I try something
    so I can revert if it does not work

--- HeistGame/Assets/AI/Actions/CopDistanceDetector.cs
+++ HeistGame/Assets/AI/Actions/CopDistanceDetector.cs
@@ -15,14 +15,42 @@ public class CopDistanceDetector : RAINAction
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 
+        Animator CopAnim = ai.Body.GetComponentInChildren<Animator>();
+
         Vector3 Target = ai.WorkingMemory.GetItem<GameObject>("varMainMovePoint").transform.position;
 
-        if (Vector3.Distance(Target, ai.Body.transform.position) < 0.3f)
-            ai.WorkingMemory.SetItem("varReadyToMove", 1);
-        else
-            ai.WorkingMemory.SetItem("varReadyToMove", 0);
-            
+        float Distance = Vector3.Distance(Target, ai.Body.transform.position);
 
+        //if (Distance < ai.Motor.CloseEnoughDistance - 0.1f)
+        //{
+        //    ai.WorkingMemory.SetItem("varReadyToMove", 1);
+        //    ai.Motor.Speed = 0;
+        //}
+        if (Distance >= 10.0f + ai.Motor.CloseEnoughDistance)
+        {
+            ai.WorkingMemory.SetItem("varReadyToMove", 0);
+            ai.Motor.DefaultSpeed = 10;
+        }
+        else
+        {
+            ai.WorkingMemory.SetItem("varReadyToMove", 0);
+            ai.Motor.DefaultSpeed = ((Distance) / (10.0f + ai.Motor.CloseEnoughDistance)) * 10;
+
+            if (ai.Motor.DefaultSpeed < 1.2f && Distance >= ai.Motor.CloseEnoughDistance)
+                ai.Motor.DefaultSpeed = 1.2f;
+        }
+
+        if (Distance < ai.Motor.CloseEnoughDistance)
+        {
+            ai.WorkingMemory.SetItem("varReadyToMove", 1);
+            CopAnim.SetFloat("Vertical", 0);
+        }
+        else
+        {
+            CopAnim.SetFloat("Vertical", (ai.Motor.DefaultSpeed) / 10);
+            ai.WorkingMemory.SetItem("varReadyToMove", 0);
+        }
+            
         return ActionResult.SUCCESS;
     }
 
