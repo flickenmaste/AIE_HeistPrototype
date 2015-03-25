0d36316a7bb3f06a8a4b4230e7ad2f70d8568697
ForceOfEvil <derekpotter@msn.com>
2015-03-25 16:33:42 -0700

    Formatting and variable name changes
    just making things easier to read

--- HeistGame/Assets/AI/Actions/CopDistanceDetector.cs
+++ HeistGame/Assets/AI/Actions/CopDistanceDetector.cs
@@ -9,53 +9,68 @@ public class CopDistanceDetector : RAINAction
 {
     public override void Start(RAIN.Core.AI ai)
     {
+        //yes it is
         base.Start(ai);
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 
+        //get the animator, the target, and the distance
         Animator CopAnim = ai.Body.GetComponentInChildren<Animator>();
 
-        Vector3 Target = ai.WorkingMemory.GetItem<GameObject>("varMainMovePoint").transform.position;
+        Vector3 Target = ai.WorkingMemory.GetItem<GameObject>("Obj_MovePoint").transform.position;
 
         float Distance = Vector3.Distance(Target, ai.Body.transform.position);
 
-        //if (Distance < ai.Motor.CloseEnoughDistance - 0.1f)
-        //{
-        //    ai.WorkingMemory.SetItem("varReadyToMove", 1);
-        //    ai.Motor.Speed = 0;
-        //}
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //this checks the distance, sets the animator speed accordingly, 
+        //and tells the squad leader (indirectly) that the cop is ready to move
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         if (Distance >= 10.0f + ai.Motor.CloseEnoughDistance)
         {
-            ai.WorkingMemory.SetItem("varReadyToMove", 0);
+            ai.WorkingMemory.SetItem("Int_ReadyToMove", 0);
             ai.Motor.DefaultSpeed = 10;
         }
+
         else
         {
-            ai.WorkingMemory.SetItem("varReadyToMove", 0);
+            ai.WorkingMemory.SetItem("Int_ReadyToMove", 0);
+
+            //use the distance and the "max speed" to get a percentage between 1 and zero
             ai.Motor.DefaultSpeed = ((Distance) / (10.0f + ai.Motor.CloseEnoughDistance)) * 10;
 
+            //if that percentage is small enough, just tell the cop to stop
             if (ai.Motor.DefaultSpeed < 0.5f || Distance <= ai.Motor.CloseEnoughDistance - 0.1f)
                 ai.Motor.DefaultSpeed = 0;
         }
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //set the cops animator walk speed to "match" the cops speed
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         if (ai.Motor.DefaultSpeed == 0)
         {
-            ai.WorkingMemory.SetItem("varReadyToMove", 1);
+            ai.WorkingMemory.SetItem("Int_ReadyToMove", 1);
             CopAnim.SetFloat("Vertical", 0);
         }
+
         else
         {
+            ai.WorkingMemory.SetItem("Int_ReadyToMove", 0);
             CopAnim.SetFloat("Vertical", (ai.Motor.DefaultSpeed) / 10);
-            ai.WorkingMemory.SetItem("varReadyToMove", 0);
         }
             
         return ActionResult.SUCCESS;
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public override void Stop(RAIN.Core.AI ai)
     {
+        //this is not a phone
         base.Stop(ai);
     }
 }
--- HeistGame/Assets/AI/Actions/PlayerDetected.cs
+++ HeistGame/Assets/AI/Actions/PlayerDetected.cs
@@ -9,32 +9,38 @@ public class PlayerDetected : RAINAction
 {
     public override void Start(RAIN.Core.AI ai)
     {
+        //nope
         base.Start(ai);
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public override ActionResult Execute(RAIN.Core.AI ai)
     {
 		//telling the cop it should break out of it's patrol state
-		ai.WorkingMemory.SetItem ("varBreak", 1);
+		ai.WorkingMemory.SetItem ("Int_Break", 1);
 
 		//get the player game object
-		GameObject player = ai.WorkingMemory.GetItem<GameObject> ("varPlayer");
+		GameObject player = ai.WorkingMemory.GetItem<GameObject> ("Obj_Player");
 
 		//store the players location from the gameobject (once the player is out of range, this will be their last location)
-		ai.WorkingMemory.SetItem("varLastPlayerPos", (Vector3)player.transform.position);
+		ai.WorkingMemory.SetItem("Vec3_LastPlayerPos", (Vector3)player.transform.position);
 
 		//reset the time to search (fixes problems a local variable has with rain scripts)
-		ai.WorkingMemory.SetItem ("varTimeToSearch", 0);
+		ai.WorkingMemory.SetItem ("Float_TimeToSearch", 0);
 
 		//make sure the cop is going to head to the players last position first
-		ai.WorkingMemory.SetItem ("varReachedLastPlayerPos", false);
+		ai.WorkingMemory.SetItem ("Bool_ReachedLastPlayerPos", false);
 
         return ActionResult.SUCCESS;
     }
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     public override void Stop(RAIN.Core.AI ai)
     {
-		ai.WorkingMemory.SetItem ("varBreak", 0);
+        //tell the squad leader to go back to what it was doing
+		ai.WorkingMemory.SetItem ("Int_Break", 0);
 
         base.Stop(ai);
     }
